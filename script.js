4dd32a10ae7d67c1db15001bc8a6ae70a59c4a1c
Victorgit14 <vbun1659@gmail.com>
2024-11-14 12:37:51 -0500

    new javascript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('Hello!');
+//Date  block
+
+const todaysDate = () => {
+    let d= new Date();
+    return "Today's date is" + d
+}
+    const dateBlock = document.getElementById("dateBlock")
+    dateBlock.innerText = todaysDate();
+    const body= document.body
+    const d=new Date();
